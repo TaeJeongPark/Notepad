aaa63cbea2af13a9f37a568cfd035f8855e4fe8a
TaeJeong <ksmpjc99@gmail.com>
2023-12-07 20:58:32 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_new_save = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_new_save
+            // 
+            this.btn_new_save.Location = new System.Drawing.Point(270, 12);
+            this.btn_new_save.Name = "btn_new_save";
+            this.btn_new_save.Size = new System.Drawing.Size(123, 34);
+            this.btn_new_save.TabIndex = 6;
+            this.btn_new_save.Text = "다른 이름으로 저장";
+            this.btn_new_save.UseVisualStyleBackColor = true;
+            this.btn_new_save.Click += new System.EventHandler(this.btn_new_save_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(141, 12);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(123, 34);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "저장";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(12, 11);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(123, 34);
+            this.btn_load.TabIndex = 4;
+            this.btn_load.Text = "불러오기";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(512, 512);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(399, 12);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(123, 34);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "초기화";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 576);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_new_save);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_new_save;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void btn_load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "텍스트 파일|*.txt|모든 파일|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                richTextBox1.Text = File.ReadAllText(filePath);
+            }
+        }*/
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "텍스트 파일|*.txt|모든 파일|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader inFp;
+                string inStr;
+
+                filePath = new FileInfo(openFileDialog.FileName).FullName;
+                inFp = new StreamReader(filePath); // 파일 오픈
+
+                while (true)
+                {
+                    inStr = inFp.ReadLine();    // 파일 읽기
+                    if (inStr == null) break;
+                    richTextBox1.Text += (inStr + "\n");   // 텍스트박스에 출력
+                }
+
+                inFp.Close();
+            }
+        }
+
+        /*private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "텍스트 파일|*.txt|모든 파일|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog.FileName;
+                    File.WriteAllText(filePath, richTextBox1.Text);
+                }
+            }
+            else
+            {
+                File.WriteAllText(filePath, richTextBox1.Text);
+            }
+        }*/
+
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            StreamWriter outFp;
+            string outStr;
+
+            outFp = new StreamWriter(filePath);
+
+            outStr = richTextBox1.Text;
+
+            outFp.WriteLine(outStr);
+
+            outFp.Close();
+            Console.WriteLine("정상적으로 파일이 저장되었습니다.");
+        }
+
+        /*private void btn_new_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "텍스트 파일|*.txt|모든 파일|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog.FileName;
+                File.WriteAllText(filePath, richTextBox1.Text);
+            }
+        }*/
+
+        private void btn_new_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "텍스트 파일|*.txt|모든 파일|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog.FileName;
+
+                StreamWriter outFp;
+                string outStr;
+
+                outFp = new StreamWriter(filePath);
+
+                outStr = richTextBox1.Text;
+
+                outFp.WriteLine(outStr);
+
+                outFp.Close();
+                Console.WriteLine("정상적으로 파일이 저장되었습니다.");
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            filePath = null;
+        }
+    }
+}
